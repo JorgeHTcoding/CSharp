a6aac5789a4718c1dd657ef4a99d0cb17f238a76
JorgeHTcoding <jht.coding@gmail.com>
2022-07-30 12:01:52 +0200

    Retos 1 y 2 modulo 1 c#

---
+++ Modulo1/app/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Ejercicio 1 
+
+Console.WriteLine("Escribe tu nombre por favor: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("¡Bienvenido a C# " + nombre + "!");
+
+//Ejercicio 2
+
+DateTime datetime = DateTime.Now;
+var hora = datetime.TimeOfDay;
+Console.WriteLine("La hora exacta ahora mismo es: " + hora);
+
